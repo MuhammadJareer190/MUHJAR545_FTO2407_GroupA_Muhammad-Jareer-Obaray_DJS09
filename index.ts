ffa73bd72f5d6e2774a4ebeff80b6357e0a4652f
Muhammad Jareer <jareerobaray@icloud.com>
2024-11-21 12:48:51 +0200

    Turple types challenge

--- index.ts
+++ index.ts
@@ -2,6 +2,9 @@
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
 // : number
+const propertiesContainer = document.querySelector('.properties')
+const footer = document.querySelector('.footer')
+
 import { showReviewTotal, populateUser } from "./utils";
 let isOpen : boolean
 
@@ -106,3 +109,18 @@ showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser)
 
 populateUser(you.isReturning, you.firstName)
 
+// Add the properties
+for (let i = 0; i < properties.length; i++) {
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.innerHTML = properties[i].title
+    const image = document.createElement('img')
+    image.setAttribute('src', properties[i].image)
+    card.appendChild(image)
+    propertyContainer.appendChild(card)
+}
+
+// use your location, your current time, and the current temperature of your
+// location
+let currentLocation: [string, string, number] = ['Cape Town', '11;30', 25]
+footer.innerHTML = currentLocation[0] + ' ' + currentLocation[1] + ' ' + currentLocation[2] + '°'
