d0c47f7fa77811ec6127df4d33ab332fff6bd1f9
Muhammad Jareer <jareerobaray@icloud.com>
2024-11-21 13:34:44 +0200

    enum types challenge

--- utils.ts
+++ utils.ts
@@ -1,9 +1,10 @@
 const returningUserDisplay = document.querySelector('#returning-user')
 const userNameDisplay = document.querySelector('#user')
 const reviewTotalDisplay = document.querySelector('#reviews')
+import { LoyaltyUser } from "./enums"
 
-export function showReviewTotal (value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
+export function showReviewTotal (value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total' + value.toString() + '/ last review by ' +
     reviewer + ' ' + iconDisplay
 }
